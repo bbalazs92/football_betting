32b6f762c0b691f0e349f515f72958fe74adf71d
Balazs <balazs.birta@gmail.com>
2021-11-11 16:54:20 +0100

    Added user registration functionality

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,27 @@
 class MainController < ApplicationController
   def index
   end
+
+  def users
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to "http://localhost:3000"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "main#index"
+  
   get "/main", to: "main#index"
+  get "/users", to: "main#users"
+  get "/new", to: "main#new"
+
+  post "/users", to: "main#create"
 end
